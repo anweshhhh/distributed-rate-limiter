4474790f2444b6e979966796ed2046cf7de4456e
anweshhhh <singh.anw@northeastern.edu>
2025-12-19 19:04:41 -0500

    feat: add window calculation and redis key formatting

--- internal/limiter/redis_fixed_window.go
+++ internal/limiter/redis_fixed_window.go
@@ -2,13 +2,16 @@ package limiter
 
 import (
 	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"time"
 
 	redis "github.com/redis/go-redis/v9"
 )
 
 // RedisFixedWindowLimiter is a distributed fixed-window rate limiter
-// backed by Redis. Behavior will be implemented incrementally.
+// backed by Redis.
 type RedisFixedWindowLimiter struct {
 	rdb        redis.UniversalClient
 	namespace  string
@@ -47,7 +50,32 @@ func NewRedisFixedWindowLimiter(
 }
 
 // Allow implements RateLimiter.
-// TODO: implement Redis-backed fixed window logic.
+// Redis logic will be added in later commits.
 func (r *RedisFixedWindowLimiter) Allow(ctx context.Context, key string) (bool, error) {
+	_ = r.currentWindowStart()
+	_ = r.redisKey(key)
 	return false, nil
+}
+
+/*
+NEW BELOW
+*/
+
+// currentWindowStart returns the unix timestamp (seconds)
+// of the current fixed window start.
+func (r *RedisFixedWindowLimiter) currentWindowStart() int64 {
+	windowSec := int64(r.windowSize.Seconds())
+	nowUnix := r.now().Unix()
+	return (nowUnix / windowSec) * windowSec
+}
+
+// redisKey returns the Redis key for the given logical key
+// in the current window.
+func (r *RedisFixedWindowLimiter) redisKey(key string) string {
+	start := r.currentWindowStart()
+
+	h := sha1.Sum([]byte(key))
+	keyHash := hex.EncodeToString(h[:])
+
+	return fmt.Sprintf("rl:%s:%s:%d", r.namespace, keyHash, start)
 }
